bb8a37ab4a64da3fab4ea21072434ca1227e3cf0
Docenko Pasha <pashadocenko@gmail.com>
2023-03-21 02:08:12 +0300

    add route "registration"

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,9 @@
 const  Routes = require("express");
 const User = require("../models/User");
 const router = new Routes();
+const bcrypt = require('bcrypt');
 
-
-router.post('/registration' , (req , res) =>{
+router.post('/registration' , async (req , res) =>{
     try {
         const {email , password} = req.body;
 
@@ -11,6 +11,11 @@ router.post('/registration' , (req , res) =>{
         if(candidate){
             return res.status(400).json({message : `User with email ${email} already exist.`})
         }
+
+        const hashPassword = await bcrypt.hash(password , 15);
+        const user = new User({email , password : hashPassword});
+        await user.save();
+        return res.json({message : "User was created."});
     }catch (e) {
         console.log(e);
         res.send({message : "Server error."});
