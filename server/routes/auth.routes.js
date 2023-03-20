ae39ab0889064eac6948864dab5282cd2edca264
Docenko Pasha <pashadocenko@gmail.com>
2023-03-20 14:29:55 +0300

    add validate in auth user (email)

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -8,6 +8,9 @@ router.post('/registration' , (req , res) =>{
         const {email , password} = req.body;
 
         const candidate = User.findOne({email});
+        if(candidate){
+            return res.status(400).json({message : `User with email ${email} already exist.`})
+        }
     }catch (e) {
         console.log(e);
         res.send({message : "Server error."});
