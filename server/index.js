d76aff9c6ef461aeface6b7668d874b2cba4ec00
Docenko Pasha <pashadocenko@gmail.com>
2023-03-21 14:00:32 +0300

    check registration to express

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
-
+const  authRouter = require("./routes/auth.routes");
 
 const app = express();
 const PORT = config.get("serverPort");
+
+
+app.use("/api/auth" , authRouter);
 
 const start = async  () =>{
     try {
