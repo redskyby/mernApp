e0d4876eba7954a70436dcd1e9e0bc450ee6dc1e
Docenko Pasha <pashadocenko@gmail.com>
2023-03-21 14:04:51 +0300

    now express can read json files.

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const  authRouter = require("./routes/auth.routes");
 const app = express();
 const PORT = config.get("serverPort");
 
-
+app.use(express.json());
 app.use("/api/auth" , authRouter);
 
 const start = async  () =>{
