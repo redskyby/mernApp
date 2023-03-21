1d35bfdffa5e2a66a1636da15bbf5206a18c7cb6
Docenko Pasha <pashadocenko@gmail.com>
2023-03-22 02:08:02 +0300

    add  jvt token in login's router

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,13 +1,14 @@
 const  Routes = require("express");
 const User = require("../models/User");
-const router = new Routes();
 const {check , validationResult} = require("express-validator")
+const config = require("config")
 const bcrypt = require('bcrypt');
-
+const  jwt = require('jsonwebtoken');
+const router = new Routes();
 
 router.post('/registration' ,
     [
-        check('email' ,"Uncorrect email").isEmail(),
+        check('email' ,"Incorrect email").isEmail(),
         check('password' ,  "Password must be longer than 3 and shorter than 12").isLength({min :3 , max :12})
     ],
     async (req , res) =>{
@@ -15,7 +16,7 @@ router.post('/registration' ,
         console.log(req.body)
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
-            return res.status(400).json({message: "Uncorrect request", errors})
+            return res.status(400).json({message: "Incorrect request", errors})
         }
 
         const {email , password} = req.body;
@@ -25,7 +26,7 @@ router.post('/registration' ,
             return res.status(400).json({message : `User with email ${email} already exist.`})
         }
         //created new user
-        const hashPassword = await bcrypt.hash(password , 15);
+        const hashPassword = await bcrypt.hash(password , 8);
         const user = new User({email , password : hashPassword});
         await user.save();
         return res.json({message : "User was created."});
@@ -35,10 +36,31 @@ router.post('/registration' ,
     }
 })
 
-router.get('/login' ,
+router.post('/login' ,
     async (req , res) =>{
         try {
+            const {email , password} = req.body;
+            const user = await  User.findOne({email});
+            if(!user){
+                return res.status(404).json({message : "User don't found."});
+            }
 
+            const isPassValid = bcrypt.compareSync(password , user.password);
+            if(!isPassValid){
+                return res.status(400).json({message : "Invalid password."});
+            }
+
+            const token = jwt.sign({id : user.id } , config.get("secretKey"), {expiresIn: "1h"});
+            return res.json({
+                token,
+                user: {
+                    id : user.id,
+                    email : user.email,
+                    diskSpace : user.diskSpace,
+                    userSpace : user.userSpace,
+                    avatar : user.avatar
+                }
+            });
         }catch (e) {
             console.log(e);
             res.send({message : "Server error."});
