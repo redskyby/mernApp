134252362ce9bd1de49a524cfd412b88ddfb904e
Docenko Pasha <pashadocenko@gmail.com>
2023-03-24 02:23:51 +0300

    add scss

--- client/src/components/ navbar/Navbar.jsx
+++ client/src/components/ navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
-function Navbar() {
+import './navbar.css'
+import Logo from '../../assets/img/navbar-logo.svg'
+import {NavLink} from "react-router-dom";
+const Navbar = () => {
     return (
-        <div></div>
+        <div className="navbar">
+            <div className="container">
+                <img src={Logo} alt="" className="navbar__logo"/>
+                <div className="navbar__header">MERN CLOUD</div>
+                <div className="navbar__login"><NavLink to="/login">Войти</NavLink></div>
+                <div className="navbar__registration"><NavLink to="/registration">Регистрация</NavLink></div>
+            </div>
+        </div>
     );
-}
+};
 
 export default Navbar;
