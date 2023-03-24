a87694bdb6d85fff0903b43c70b7dcf81c662d53
Docenko Pasha <pashadocenko@gmail.com>
2023-03-25 02:33:48 +0300

    add Input.jsx and axios

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './registration.css'
+import Input from "../../utils/input/Input";
+import {registration} from "../../actions/user";
 
-function Registration() {
+const Registration = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
-        <div></div>
+        <div className='registration'>
+            <div className="registration__header">Регистрация</div>
+            <Input value={email} setValue={setEmail} type="text" placeholder="Введите email..."/>
+            <Input value={password} setValue={setPassword} type="password" placeholder="Введите пароль..."/>
+            <button className="registration__btn" onClick={() => registration(email, password)}>Войти</button>
+        </div>
     );
-}
+};
 
 export default Registration;
---
+++ client/src/utils/input/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './input.css'
+
+const Input = (props) => {
+    return (
+        <input
+               onChange={(event)=> props.setValue(event.target.value)}
+               value={props.value}
+               type={props.type}
+               placeholder={props.placeholder}
+        />
+    );
+};
+
+export default Input;
