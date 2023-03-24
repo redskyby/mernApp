51f153a532a885684406c27bbd3d06798e30d801
Docenko Pasha <pashadocenko@gmail.com>
2023-03-25 02:36:12 +0300

    add Input.jsx and axios

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const  authRouter = require("./routes/auth.routes");
-
 const app = express();
 const PORT = config.get("serverPort");
+const corsMiddleware = require('./middleware/cors.middleware')
 
+app.use(corsMiddleware)
 app.use(express.json());
 app.use("/api/auth" , authRouter);
 
