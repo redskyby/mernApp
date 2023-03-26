d796e350c77363048806b0d7a1520550421ca5b9
Docenko Pasha <pashadocenko@gmail.com>
2023-03-26 23:50:33 +0300

    init File.js ( new model)

--- server/models/File.js
+++ server/models/File.js
@@ -1,1 +1,15 @@
 const {Schema , model , ObjectId} = require("mongoose");
+
+
+const File = new Schema({
+    name : {type : String , require : true},
+    type : {type : String , require : true},
+    accessLink :{type : String },
+    size: {type : Number , default : 0},
+    path : {type : String , default: ''},
+    user :{type : ObjectId , ref : 'User'},
+    parent : {type : ObjectId , ref : 'File'},
+    child : [{type : ObjectId , ref : 'File'}]
+})
+
+module.exports = model('File' , File)
