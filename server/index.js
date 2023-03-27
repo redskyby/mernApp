3bd1b3ee6c92306760e5261d4a154bef5ceb9903
Docenko Pasha <pashadocenko@gmail.com>
2023-03-27 22:58:38 +0300

    init file.routes.js and add route in express

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
-const  authRouter = require("./routes/auth.routes");
+const authRouter = require("./routes/auth.routes");
+const fileRouter = require("./routes/file.routes");
 const app = express();
 const PORT = config.get("serverPort");
 const corsMiddleware = require('./middleware/cors.middleware')
 
 app.use(corsMiddleware)
 app.use(express.json());
-app.use("/api/auth" , authRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/files", fileRouter);
 
-const start = async  () =>{
+
+const start = async () => {
     try {
         await mongoose.connect(config.get('dbUrl'));
 
         app.listen(PORT, () => {
-            console.log('Server started on post ' , PORT);
+            console.log('Server started on post ', PORT);
         });
-    }catch (e) {
+    } catch (e) {
         console.log(e.message);
     }
 }
---
+++ server/routes/file.routes.js
@@ -0,0 +1,7 @@
+const Router = require('express');
+const router = new Router();
+const authMiddleware = require('../middleware/auth.middleware');
+
+
+
+module.exports = router;
