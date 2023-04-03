02506a340015afdac0af33f556455d7a5eb63bbd
Docenko Pasha <pashadocenko@gmail.com>
2023-04-04 02:59:08 +0300

    init class fileService

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 const File = require('../models/File');
-
+const config = require('config')
 
 class FileService{
-    createDir(){
-
+    createDir(file) {
+        const filePath = `${config.get('filePath')}\\${file.user}\\${file.path}`
+        return new Promise(((resolve, reject) => {
+            try {
+                if (!fs.existsSync(filePath)) {
+                    fs.mkdirSync(filePath)
+                    return resolve({message: 'File was created'})
+                } else {
+                    return reject({message: "File already exist"})
+                }
+            } catch (e) {
+                return reject({message: 'File error'})
+            }
+        }))
     }
 }
 
