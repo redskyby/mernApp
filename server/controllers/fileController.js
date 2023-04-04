6c5716de62d2be7e6e4a9fe8c180ec6519363c09
Docenko Pasha <pashadocenko@gmail.com>
2023-04-05 02:44:31 +0300

    init fileContoller.js

---
+++ server/controllers/fileController.js
@@ -0,0 +1,8 @@
+const fileService = require('../services/fileService');
+const  User = require('../models/User');
+const  File = require('../models/File');
+
+class fileController{
+
+}
+module.exports = new fileController();
