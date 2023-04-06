8c7d72fbcebbfde0b923fe1c684269c17007d90f
Docenko Pasha <pashadocenko@gmail.com>
2023-04-07 01:50:02 +0300

    fix some bags

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,12 +1,11 @@
 const fileService = require('../services/fileService');
-const  User = require('../models/User');
 const  File = require('../models/File');
 
 class fileController{
         async createDir(req , res){
             try {
-                const  {name , type , parents} = req.body;
-                const  file = new File({name , type , parents , user : user.id});
+                const  {name , type , parent} = req.body;
+                const  file = new File({name , type , parent , user : req.user.id});
                 const parentFile = await  File.findOne({_id : parent});
                 if(!parentFile){
                         file.path = name;
