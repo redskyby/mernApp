7a9105b2b4b7bb96d8193abe18e95f0a24cb169d
Docenko Pasha <pashadocenko@gmail.com>
2023-04-11 22:28:20 +0300

    init file.js (action)

---
+++ client/src/actions/file.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export function getFiles(dirId){
+    return async dispatch =>{
+        try {
+            
+        }catch (e) {
+            alert(e.response.data.message);
+        }
+    }
+}
