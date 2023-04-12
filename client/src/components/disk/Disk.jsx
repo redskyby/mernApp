3f55c67dca9da33540d38e9a2925d4ede1c94a80
Docenko Pasha <pashadocenko@gmail.com>
2023-04-12 23:11:09 +0300

    start init Disk.jsx

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import file from "../../redux/slice/FileSlice";
 import {getFiles} from "../../actions/file";
+import FileList from "./filelist/FileList";
 
 function Disk() {
     const dispatch = useDispatch();
@@ -11,7 +12,13 @@ function Disk() {
         dispatch(getFiles(currentDir));
     }, [currentDir]);
     return (
-        <div>Disk</div>
+        <div className="disk">
+            <div className="disk_btns">
+                <button className="disk_back">Назад</button>
+                <button className="disk_create">Создать папку</button>
+            </div>
+            <FileList />
+        </div>
     );
 }
 
