02c4a1b503095b68ab9763fc09d7b2497c627ed3
Docenko Pasha <pashadocenko@gmail.com>
2023-04-18 14:10:44 +0300

    fix import in file.scss

--- client/src/components/disk/filelist/FileList.jsx
+++ client/src/components/disk/filelist/FileList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {useSelector} from "react-redux";
 import './fileList.css'
-function FileList() {
-    const files = useSelector(state => state.fileToolkit.files);
+import File from "./file/File";
 
+function FileList() {
+    // const files = useSelector(state => state.fileToolkit.files).map(file => <File />);
+    const files = [{_id: 1, name: 'direc', type: 'dir', size: '5gb', date: '20.02.2020'},
+        {_id: 1, name: 'direc', type: 'dir', size: '5gb', date: '20.02.2020'}
+    ].map(file => <File key={file.id}/>);
     return (
         <div className='filelist'>
             <div className="filelist__header">
@@ -11,7 +15,9 @@ function FileList() {
                 <div className="filelist__date">Дата</div>
                 <div className="filelist__size">Размер</div>
             </div>
-
+            {
+                files
+            }
         </div>
     );
 }
--- client/src/components/disk/filelist/file/File.jsx
+++ client/src/components/disk/filelist/file/File.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './file.css';
-import dirLogo from '../../../../assets/img/dir.svg'
-import fileLogo from '../../../../assets/img/file.svg'
+import dirLogo from '../../../../assets/dir.svg';
+import fileLogo from '../../../../assets/file.svg';
 
 function File({file}) {
     return (
-        <div className='file'>
+       /* <div className='file'>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0,10)}</div>
             <div className="file__size">{file.size}</div>
         </div>
-
+*/<></>
     );
 }
 
