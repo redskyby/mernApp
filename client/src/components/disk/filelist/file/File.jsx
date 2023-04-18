4199e839b3231d1c796ef50d15052d3e42f9344d
Docenko Pasha <pashadocenko@gmail.com>
2023-04-18 14:12:11 +0300

    init File.jsx

--- client/src/components/disk/filelist/file/File.jsx
+++ client/src/components/disk/filelist/file/File.jsx
@@ -5,13 +5,13 @@ import fileLogo from '../../../../assets/file.svg';
 
 function File({file}) {
     return (
-       /* <div className='file'>
+       <div className='file'>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0,10)}</div>
             <div className="file__size">{file.size}</div>
         </div>
-*/<></>
+
     );
 }
 
