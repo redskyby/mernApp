26d8fb4f05b4c34c3f273e14b87e1b45f92a8874
Docenko Pasha <pashadocenko@gmail.com>
2023-04-18 13:46:01 +0300

    fix bags and add images (assets)

--- client/src/components/disk/filelist/file/File.jsx
+++ client/src/components/disk/filelist/file/File.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './file.css';
-import {useSelector} from "react-redux";
+import dirLogo from '../../../../assets/img/dir.svg'
+import fileLogo from '../../../../assets/img/file.svg'
 
-function File() {
-    const files = useSelector(state => state.fileToolkit.files);
-
+function File({file}) {
     return (
+        <div className='file'>
+            <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
+            <div className="file__name">{file.name}</div>
+            <div className="file__date">{file.date.slice(0,10)}</div>
+            <div className="file__size">{file.size}</div>
+        </div>
 
-        <></>
     );
 }
 
