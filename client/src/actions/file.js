8cfc21b12c0cd6067293f9d435bc5abed3e59c01
Docenko Pasha <pashadocenko@gmail.com>
2023-04-22 17:32:45 +0300

    add function. which can create folder. (file.js)

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -14,3 +14,22 @@ export function getFiles(dirId){
         }
     }
 }
+
+export function createDir(dirId , name){
+    return async dispatch =>{
+        try {
+            const response = await axios.post(`http://localhost:5000/api/files`,{
+                name,
+                parent : dirId,
+                type : 'dir'
+
+            },{
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+            });
+            dispatch(SET_FILES(response.data));
+            //console.log(response.data);
+        }catch (e) {
+            alert(e.response.data.message);
+        }
+    }
+}
