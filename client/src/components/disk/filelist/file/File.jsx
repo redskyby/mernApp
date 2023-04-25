0f19dad8a74b956b36c40b4fdb5269f9f9703bd1
Docenko Pasha <pashadocenko@gmail.com>
2023-04-25 23:29:59 +0300

    add function openHandler in File.jsx

--- client/src/components/disk/filelist/file/File.jsx
+++ client/src/components/disk/filelist/file/File.jsx
@@ -4,8 +4,15 @@ import dirLogo from '../../../../assets/dir.svg';
 import fileLogo from '../../../../assets/file.svg';
 
 function File({file}) {
+    function openHadler() {
+
+    }
+
     return (
-       <div className='file'>
+       <div
+           className='file'
+           onClick={() => openHadler()}
+       >
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0,10)}</div>
