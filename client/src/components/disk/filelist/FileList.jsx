b99f42f86619d418c4ca9d2afd0710752db8d655
Docenko Pasha <pashadocenko@gmail.com>
2023-05-15 23:48:36 +0300

    beautiful))

--- client/src/components/disk/filelist/FileList.jsx
+++ client/src/components/disk/filelist/FileList.jsx
@@ -4,8 +4,9 @@ import './fileList.css'
 import File from "./file/File";
 
 function FileList() {
-    const [checkPlace , setCheckPlace] = useState(false);
-    const files = useSelector(state => state.fileToolkit.files).map(file => <File key={file._id} file={file} setCheckPlace={setCheckPlace}/>);
+    const [checkPlace, setCheckPlace] = useState(false);
+    const files = useSelector(state => state.fileToolkit.files).map(file => <File key={file._id} file={file}
+                                                                                  setCheckPlace={setCheckPlace}/>);
 
     return (
         <div className='filelist'>
@@ -13,7 +14,7 @@ function FileList() {
                 className="filelist__header"
             >
                 <div className="filelist__name">Название</div>
-                <div className={checkPlace ? "filelist__change__place__date" : "filelist__date" }  >Дата</div>
+                <div className={checkPlace ? "filelist__change__place__date" : "filelist__date"}>Дата</div>
                 <div className={checkPlace ? "filelist__change__place__size" : "filelist__size"}>Размер</div>
             </div>
             {files}
