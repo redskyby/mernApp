76c228e8d3903dc96cf2cc5c3ae5e09a73c62527
Docenko Pasha <pashadocenko@gmail.com>
2023-05-15 20:55:04 +0300

    add route for download

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -3,8 +3,9 @@ const router = new Router();
 const authMiddleware = require('../middleware/auth.middleware');
 const fileController = require('../controllers/fileController');
 
-router.post('', authMiddleware, fileController.createDir)
-router.post('/upload', authMiddleware, fileController.upLoadFile)
-router.get('', authMiddleware, fileController.getFiles)
+router.post('', authMiddleware, fileController.createDir);
+router.post('/upload', authMiddleware, fileController.upLoadFile);
+router.get('', authMiddleware, fileController.getFiles);
+router.get('/download', authMiddleware, fileController.downloadFile);
 
 module.exports = router;
