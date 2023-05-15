6037300c393a56de6a01b1b37587e3e74c9ef85b
Docenko Pasha <pashadocenko@gmail.com>
2023-05-15 22:49:33 +0300

    init new action downloadFile in file.js

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -51,9 +51,7 @@ export function upLoadFile(file , dirId) {
                             let progress = Math.round((progressEvent.loaded * 100) / totalLength);
                             console.log(progress);
                         }
-
                 }
-
             });
             dispatch(ADD_FILE(response.data));
         } catch (e) {
@@ -62,6 +60,20 @@ export function upLoadFile(file , dirId) {
     }
 }
 
-export function downloadFile(){
-
+export async function downloadFile(file){
+        const response = await fetch(`http://localhost:5000/api/files/download?id=${file.id}`,{
+            headers : {
+                Authorization : `Bearer ${localStorage.getItem('token')} `
+            }
+        })
+        if(response.status === 200){
+            const blob = await response.blob();
+            const downloadUrl = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = downloadUrl;
+            link.download = file.name;
+            document.appendChild(link);
+            link.click();
+            link.remove();
+        }
 }
