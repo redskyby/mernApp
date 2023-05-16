a6982542d08e0926e61175e4a028ce780c878204
Docenko Pasha <pashadocenko@gmail.com>
2023-05-16 23:46:58 +0300

    mend bags in fileController.js

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -64,11 +64,15 @@ class fileController{
                 file.mv(path);
 
                 const type = file.name.split('.').pop();
+                let filePath = file.name;
+                if(parent){
+                    filePath = parent.path + "\\" + file.name;
+                }
                 const dbFile = new File({
                     name : file.name,
                     type,
                     size : file.size,
-                    path : parent?.path,
+                    path : filePath,
                     parent : parent?._id,
                     user : user._id
                 })
