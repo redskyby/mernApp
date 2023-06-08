0fe2419779791fa3a086c4f8e5dcaf30c2e8153b
Docenko Pasha <pashadocenko@gmail.com>
2023-06-08 23:06:17 +0300

    init upload (Slice) in redux

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,10 +1,12 @@
 import {combineReducers , configureStore} from "@reduxjs/toolkit";
 import user from "./slice/UserSlice"
 import file from "./slice/FileSlice";
+import upload from "./slice/UploadSlice";
 
 const rootReducer = combineReducers({
     userToolkit : user,
-    fileToolkit : file
+    fileToolkit : file,
+    uploadToolKit : upload
 })
 
 export const store = configureStore({
--- client/src/redux/slice/UploadSlice.js
+++ client/src/redux/slice/UploadSlice.js
@@ -1,3 +1,34 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+const initialState = {
+    isVisitable: true,
+    files: [],
+}
 
+const upload = createSlice({
+    name: "upload",
+    initialState,
+    reducers: {
+        SHOW_UPLOADER : (state) =>{
+            state.isVisitable = true;
+        },
+        HIDE_UPLOADER : (state) =>{
+            state.isVisitable = false;
+        },
+        ADD_UPLOADER_FILE : (state, action)=>{
+            state.file = [...state.files ,{...action.payload , id : state.files.length}];
+        },
+        REMOVE_UPLOAD_FILE: (state, action)=>{
+            state.files = [...state.files.filter(file => file !== action.payload)]
+        }
+    }
+});
+
+
+export default upload.reducer;
+export const {
+    SHOW_UPLOADER,
+    HIDE_UPLOADER,
+    ADD_UPLOADER_FILE,
+    REMOVE_UPLOAD_FILE
+} = upload.actions;
