94b918546a41873b81df12a6d622cf7fbf14c2e0
Docenko Pasha <pashadocenko@gmail.com>
2023-06-08 23:12:48 +0300

    Component Uploader.jsx is finished.

--- client/src/components/disk/uploader/Uploader.jsx
+++ client/src/components/disk/uploader/Uploader.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import "./uploader.css";
 import UploadFile from "./UploadFile";
+import {useDispatch, useSelector} from "react-redux";
+import {HIDE_UPLOADER} from "../../../redux/slice/UploadSlice";
 
 function Uploader() {
-    const files = [];
-    return (
+    const files = useSelector(state => state.uploadToolKit.files);
+    const isVisible = useSelector(state => state.uploadToolKit.isVisitable);
+    const dispatch = useDispatch();
+
+    return ( isVisible &&
         <div>
             <div className="uploader">
                 <div className="uploader__header">
                     <div className="uploader__title">Загрузки</div>
-                    <button type="button">X</button>
+                    <button type="button"
+                            onClick={()=>dispatch(HIDE_UPLOADER())}
+                    >X</button>
                 </div>
             </div>
             {
