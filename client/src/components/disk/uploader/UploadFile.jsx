c95d28c514f2af7b2064f4416012d36bb284b217
Docenko Pasha <pashadocenko@gmail.com>
2023-06-08 22:46:34 +0300

    init UploadFile.jsx

--- client/src/components/disk/uploader/UploadFile.jsx
+++ client/src/components/disk/uploader/UploadFile.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-
-function UploadFile(file) {
+import "./uploader.css";
+function UploadFile({file}) {
     return (
-        <div></div>
+        <div className="upload-file">
+            <div className="upload-file__header">
+                <div className="upload-file__name">
+                    {file.name}
+                </div>
+                <button
+                    type="button"
+                    className="upload-file__remove"
+                >X</button>
+            </div>
+            <div className="upload-file__progress-bar">
+                <div className="upload-file__progress-bar"/>
+                <div className="upload-file__progress-bar">{file.progress}</div>
+            </div>
+        </div>
     );
 }
 
