1e702c253594b3c763daf2e2bcc6b31012855c6a
Docenko Pasha <pashadocenko@gmail.com>
2023-06-10 23:27:28 +0300

    add remove function in UploadFile.jsx

--- client/src/components/disk/uploader/UploadFile.jsx
+++ client/src/components/disk/uploader/UploadFile.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import "./uploader.css";
+import {useDispatch} from "react-redux";
+import {REMOVE_UPLOAD_FILE} from "../../../redux/slice/UploadSlice";
 function UploadFile({file}) {
+    const dispatch = useDispatch();
+
     return (
         <div className="upload-file">
             <div className="upload-file__header">
@@ -10,6 +14,7 @@ function UploadFile({file}) {
                 <button
                     type="button"
                     className="upload-file__remove"
+                    onClick={()=> dispatch(REMOVE_UPLOAD_FILE(file.id))}
                 >X</button>
             </div>
             <div className="upload-file__progress-bar">
