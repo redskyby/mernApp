3e938a3fb88472318bc0816917a293fd1a198194
Docenko Pasha <pashadocenko@gmail.com>
2023-06-11 16:25:30 +0300

    add animation in FileList.jsx

--- client/src/components/disk/filelist/FileList.jsx
+++ client/src/components/disk/filelist/FileList.jsx
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import {useSelector} from "react-redux";
 import './fileList.css'
 import File from "./file/File";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 function FileList() {
     const [checkPlace, setCheckPlace] = useState(false);
-    const files = useSelector(state => state.fileToolkit.files).map(file => <File key={file._id} file={file}
-                                                                                  setCheckPlace={setCheckPlace}/>);
+    const files = useSelector(state => state.fileToolkit.files)
 
     return (
         <div className='filelist'>
@@ -17,7 +17,20 @@ function FileList() {
                 <div className={checkPlace ? "filelist__change__place__date" : "filelist__date"}>Дата</div>
                 <div className={checkPlace ? "filelist__change__place__size" : "filelist__size"}>Размер</div>
             </div>
-            {files}
+            <TransitionGroup>
+                {
+                    files.map(file =>
+                        <CSSTransition
+                            key={file._id}
+                            timeout={500}
+                            classNames={'file'}
+                            exit={false}
+                        >
+                            <File file={file} setCheckPlace={setCheckPlace}/>
+                        </CSSTransition>
+                    )
+                }
+            </TransitionGroup>
         </div>
     );
 }
