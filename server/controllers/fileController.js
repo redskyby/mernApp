93ea6c92ca67e1d85a853655172bffa535ec729d
Docenko Pasha <pashadocenko@gmail.com>
2023-06-18 21:53:51 +0300

    add "sort" in getFiles (fileController.js)

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -30,7 +30,22 @@ class fileController{
 
     async getFiles(req, res) {
         try {
-            const files = await File.find({user: req.user.id, parent: req.query.parent})
+            const {sort} = req.query;
+            let files;
+            switch (sort) {
+                case 'name':
+                    files = await File.find({user: req.user.id, parent: req.query.parent}).sort({name:1});
+                    break;
+                case 'type':
+                    files = await File.find({user: req.user.id, parent: req.query.parent}).sort({type:1});
+                    break;
+                case 'date':
+                    files = await File.find({user: req.user.id, parent: req.query.parent}).sort({date:1});
+                    break;
+                default:
+                    files = await File.find({user: req.user.id, parent: req.query.parent});
+                    break;
+            }
             return res.json(files)
         } catch (e) {
             console.log(e)
