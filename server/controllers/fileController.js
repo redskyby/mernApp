1e7a8c435edf0fd7348111f57d29cee5383691f3
Docenko Pasha <pashadocenko@gmail.com>
2023-07-02 01:12:51 +0300

    add new method (deleteAvatar)

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -160,6 +160,19 @@ class fileController {
             return res.status(400).json({message: "Upload avatar error."});
         }
     }
+
+    async deleteAvatar(req , res){
+        try{
+            const user = await User.findOne(req.use.id);
+            fs.unlinkSync(config.get('staticPath') + '\\' + user.avatar);
+            user.avatar = null;
+            await  user.save();
+            return res.json(user);
+        }catch (e) {
+            console.log(e);
+            return res.status(400).json({message: "Delete avatar error."});
+        }
+    }
 }
 
 module.exports = new fileController();
