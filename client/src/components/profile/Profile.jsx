3889a75d4112647a5578f49e1ae8a724bdb7c8af
Docenko Pasha <pashadocenko@gmail.com>
2023-07-03 17:56:50 +0300

    The little refactor url-request.

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -3,14 +3,10 @@ import React from 'react';
 const Profile = () => {
     return (
         <div>
-<<<<<<< HEAD
             <button
                 type="button">Удалить аватар
             </button>
             <input type="file" placeholder="Загрузить аватар"/>
-=======
-            
->>>>>>> origin/main
         </div>
     );
 };
